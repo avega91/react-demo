4487bdebdb021cbea22d6c49c3494da1a91de10b
Alan Vega <navm_060@hotmail.com>
2020-03-17 13:57:21 -0600

    Updates

---
+++ precache-manifest.7723c1ba2b0042629f0b4f2baf1c42db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a2499e58f389ef80ca9d80a196e335",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "91fbdba2fdf3a87010cc",
+    "url": "/react-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44d125f9cc40551f9304",
+    "url": "/react-demo/static/js/2.77d70802.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-demo/static/js/2.77d70802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91fbdba2fdf3a87010cc",
+    "url": "/react-demo/static/js/main.d5dfdd64.chunk.js"
+  },
+  {
+    "revision": "90d2341f47826787710c",
+    "url": "/react-demo/static/js/runtime-main.23b42400.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
